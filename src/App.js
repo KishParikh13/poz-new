72318b1eb2f8aec0c28a57e3e75578b06fcb5f2f
KishParikh13 <kaparikh@mail.smu.edu>
2023-05-26 15:34:28 -0700

    add auth, settings, more

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React, { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, useParams } from "react-router-dom";
 import Home from './views/Home';
 import Note from './views/Note';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  useParams,
-} from "react-router-dom";
 import Layout from './views/Layout';
+import Settings from './views/Settings';
+import { SignInOrUp } from './views/Landing';
 
 function App() {
 
   return (
     <Router>
-      <Layout>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/" element={<SignInOrUp />} />
+          <Route path="/settings" element={<Settings />} />
           <Route path="notes/:id" element={<Note />} />
         </Routes>
-      </Layout>
     </Router>
   );
 }
